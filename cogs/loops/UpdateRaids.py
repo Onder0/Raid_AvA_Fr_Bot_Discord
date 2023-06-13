6fa8b1c134b0747feca57a2d33be8b01805e7269
Onder0 <onderpout@gmail.com>
2023-06-13 19:29:06 +0200

    Zabrak modif : Updates raids Fait

--- cogs/loops/UpdateRaids.py
+++ cogs/loops/UpdateRaids.py
@@ -29,63 +29,38 @@ class UpdateRaids(commands.Cog):
 
 async def verif_liste_raid(self):
     bot_id = settings.bot_id
-    chan_raid = self.bot.get_channel(settings.chan_annonces_raids)
-
-    embed = await embed_liste_raid(self)
-
-    if embed is None:
-        return
-
-    bot_message_id = None
-    messages = [message async for message in chan_raid.history()]
-
-    for index, message in enumerate(messages):
-        if message.author.id == bot_id:
-            bot_message_id = message.id
-            break
-
-    if bot_message_id == None:
-        await chan_raid.send(embed=embed)
-    elif index != 0:
-        await messages[index].delete()
-        await chan_raid.send(embed=embed)
-    else:
-        await message.edit(embed=embed)
-
-
-async def embed_liste_raid(self):
+    chan_raid_bw = self.bot.get_channel(settings.chan_annonces_raids_bw)
+    chan_raid_fs = self.bot.get_channel(settings.chan_annonces_raids_fs)
     server_id = settings.guild_id[0]
     api_key = settings.raid_helper_token
 
     url = f"https://raid-helper.dev/api/v2/servers/{server_id}/events"
     headers = {"Authorization": api_key}
 
-    # Envoyer une requête GET à l'API
     response = requests.get(url, headers=headers)
 
     # Vérifier si la requête a réussi
     if response.status_code == 200:
-        # Charger la réponse JSON
         data = response.json()
 
-        chan_raid = self.bot.get_channel(settings.chan_annonces_raids)
-        # Filtrer les événements pour ne conserver que ceux dans le channel #annonce-raid
-        events = [
-            event for event in data["postedEvents"] if int(event["channelId"]) == chan_raid.id
+        events_bw = [
+            event for event in data["postedEvents"] if int(event["channelId"]) == chan_raid_bw.id
+        ]
+        events_fs = [
+            event for event in data["postedEvents"] if int(event["channelId"]) == chan_raid_fs.id
         ]
 
-        # Tri des événements par date
-        events = sorted(events, key=lambda event: event["startTime"])
+        events_bw = sorted(events_bw, key=lambda event: event["startTime"])
+        events_fs = sorted(events_bw, key=lambda event: event["startTime"])
 
-        # Création d'un dictionnaire pour stocker les événements par jour
-        events_by_day = {}
+        events_by_day_bw = {}
+        events_by_day_fs = {}
 
-        guild = chan_raid.guild
+        guild = chan_raid_bw.guild
         calendrier = nextcord.utils.get(guild.emojis, name="calendrier")
         groupe = nextcord.utils.get(guild.emojis, name="groupe")
 
-        # Parcourir la liste des événements filtrés
-        for event in events:
+        for event in events_bw:
             start_time = event["startTime"]
             event_date = datetime.datetime.fromtimestamp(start_time).strftime("%d-%m-%Y")
             event_hour = datetime.datetime.fromtimestamp(start_time).strftime("%H:%M")
@@ -96,39 +71,95 @@ async def embed_liste_raid(self):
                 f"https://discord.com/channels/{server_id}/{event['channelId']}/{event['id']}"
             )
 
-            # Si la date de début de l'événement n'est pas déjà présente dans le dictionnaire, l'ajouter avec une liste vide comme valeur
-            if event_date not in events_by_day:
-                events_by_day[event_date] = []
+            if event_date not in events_by_day_bw:
+                events_by_day_bw[event_date] = []
 
-            # Ajouter l'événement à la liste des événements pour la date de début correspondante
-            events_by_day[event_date].append(
+            events_by_day_bw[event_date].append(
+                f"> `{event_hour}`  {groupe}  `{event_signup}` **[{event_title}]({event_url})** <t:{start_time}:R>"
+            )
+        for event in events_fs:
+            start_time = event["startTime"]
+            event_date = datetime.datetime.fromtimestamp(start_time).strftime("%d-%m-%Y")
+            event_hour = datetime.datetime.fromtimestamp(start_time).strftime("%H:%M")
+            event_signup = event["signUpsAmount"]
+            event_title = event["title"]
+
+            event_url = (
+                f"https://discord.com/channels/{server_id}/{event['channelId']}/{event['id']}"
+            )
+
+            if event_date not in events_by_day_fs:
+                events_by_day_fs[event_date] = []
+
+            events_by_day_fs[event_date].append(
                 f"> `{event_hour}`  {groupe}  `{event_signup}` **[{event_title}]({event_url})** <t:{start_time}:R>"
             )
 
-        # Création de l'embed Nextcord
-        embed = nextcord.Embed(title="Liste des Raids :", color=nextcord.Color.teal())
+        embed_bw = nextcord.Embed(title="Liste des Raids :", color=nextcord.Color.orange())
+        embed_fs = nextcord.Embed(title="Liste des Raids :", color=nextcord.Color.light_grey())
 
         # Parcourir le dictionnaire des événements par jour et ajouter un champ à l'embed pour chaque jour avec la liste des événements pour ce jour
-        for event_date, event_list in events_by_day.items():
+        for event_date, event_list in events_by_day_bw.items():
             # Joindre les événements pour ce jour en les séparant par des sauts de ligne
             events_str = "\n".join(event_list)
             # Ajouter le champ à l'embed avec la date de début comme nom et la liste des événements pour ce jour comme valeur
-            embed.add_field(name=f"{calendrier}  {event_date}", value=events_str, inline=False)
+            embed_bw.add_field(name=f"{calendrier}  {event_date}", value=events_str, inline=False)
+        for event_date, event_list in events_by_day_fs.items():
+            events_str = "\n".join(event_list)
+            embed_fs.add_field(name=f"{calendrier}  {event_date}", value=events_str, inline=False)
 
-        if len(events) == 0:
-            embed.add_field(name="Il n'y a pas de raids actuellement", value="\n", inline=False)
+        if len(events_bw) == 0:
+            embed_bw.add_field(name="Il n'y a pas de raids actuellement", value="\n", inline=False)
+        if len(events_fs) == 0:
+            embed_fs.add_field(name="Il n'y a pas de raids actuellement", value="\n", inline=False)
 
         # Ajouter le dernier champ avec des informations supplémentaires
-        embed.add_field(
+        embed_bw.add_field(
             name="",
             value=f"Les infos sont actualisées toutes les {settings.time_loop_update_raids} minutes.",
         )
-
-        return embed
+        embed_fs.add_field(
+            name="",
+            value=f"Les infos sont actualisées toutes les {settings.time_loop_update_raids} minutes.",
+        )
     else:
         logger.warning(
             f"Échec de la requête à l'API Raid Helper => Status : {response.status_code}\n"
         )
+
+    if embed_bw is None and embed_fs == None:
+        return
+
+    bot_message_id_bw = None
+    bot_message_id_fs = None
+
+    messages_bw = [message async for message in chan_raid_bw.history()]
+    messages_fs = [message async for message in chan_raid_bw.history()]
+
+    for index, message in enumerate(messages_bw):
+        if message.author.id == bot_id:
+            bot_message_id_bw = message.id
+            break
+    for index, message in enumerate(messages_fs):
+        if message.author.id == bot_id:
+            bot_message_id_fs = message.id
+            break
+
+    if bot_message_id_bw == None:
+        await chan_raid_bw.send(embed=embed_bw)
+    elif index != 0:
+        await messages_bw[index].delete()
+        await chan_raid_bw.send(embed=embed_bw)
+    else:
+        await message.edit(embed=embed_bw)
+
+    if bot_message_id_fs == None:
+        await chan_raid_fs.send(embed=embed_fs)
+    elif index != 0:
+        await messages_fs[index].delete()
+        await chan_raid_fs.send(embed=embed_fs)
+    else:
+        await message.edit(embed=embed_fs)
 
 
 def setup(bot):
