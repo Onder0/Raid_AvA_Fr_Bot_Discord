3dd7c4d6a83074c8d3445121b733c5cfd246524d
Onder0 <onderpout@gmail.com>
2023-06-14 01:09:41 +0200

    nouvelle DB rankup

--- cogs/management/Model_Rankup.py
+++ cogs/management/Model_Rankup.py
@@ -7,18 +7,16 @@ class Rankup(Model):
     nom_membre = TextField(default="")
     off_tank = SmallIntegerField(default=0)
     healer = SmallIntegerField(default=0)
-    grand_arcane = SmallIntegerField(default=0)
-    arcane = SmallIntegerField(default=0)
+    arcanes = SmallIntegerField(default=0)
     souchefer = SmallIntegerField(default=0)
-    mande_tenebre = SmallIntegerField(default=0)
-    epee_tranchante = SmallIntegerField(default=0)
-    brise_royaume = SmallIntegerField(default=0)
-    chasse_esprit = SmallIntegerField(default=0)
-    spectre = SmallIntegerField(default=0)
-    frost = SmallIntegerField(default=0)
-    fire = SmallIntegerField(default=0)
-    arbalete = SmallIntegerField(default=0)
+    support = SmallIntegerField(default=0)
+    hp_cut = SmallIntegerField(default=0)
+    dps = SmallIntegerField(default=0)
     scout = SmallIntegerField(default=0)
 
     class Meta:
         database = database
+
+
+from peewee import *
+from database.database import database
